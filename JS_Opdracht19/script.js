e7750a742b4ac7403186b65e7bf375b25fa1fb68
BDirkson <69297382+BDirkson@users.noreply.github.com>
2020-09-17 15:46:05 +0200

    Add files via upload

---
+++ JS_Opdracht19/script.js
@@ -0,0 +1,16 @@
+console.log("Hello Winc Academy");
+let name ='bart';
+
+console.log(name);
+let number = 4+4;
+console.log(number);
+
+let age = 'fourtythree';
+console.log(age)
+
+function myFunction() {
+let x = 10;
+let y = 5;
+console.log(x+y);
+
+}
