7ef0fa05709ee068fcade6997aaf4e414e9fbbb7
BDirkson <69297382+BDirkson@users.noreply.github.com>
2020-09-21 13:42:27 +0200

    Add files via upload

---
+++ JS_Opdracht20/script.js
@@ -0,0 +1,8 @@
+console.log("Hallo Winc Academy studenten") 
+/*dit zijn grote sommen als test die we uitzetten dmv block comment
+let multiplication = 1230941 * 1823794;
+console.log("multiplication:", multiplication);
+
+let division = 637263 / 54;
+console.log("division:", division);
+*/
