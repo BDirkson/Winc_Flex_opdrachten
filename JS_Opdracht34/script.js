53b7ff8367740f2afa35e0e629389a1e45e53ae7
BDirkson <69297382+BDirkson@users.noreply.github.com>
2020-10-06 19:25:19 +0200

    Add files via upload

---
+++ JS_Opdracht34/script.js
@@ -0,0 +1,37 @@
+//A
+// Opdracht
+// zet de functie om naar een arrow functie
+ const ikRockArrowFunctions = function () {
+    console.log("Joe, ik rock de arrow functions!");
+  };
+
+let ikRockArrowFunctions = () => {
+    console.log("Joe, ik rock de arrow functions!");
+};
+//B
+//Opdracht: schrijf de onderstaande functie om naar een arrow functie op 1 regel.
+const fivePlusSeven = function () {
+    return 5 + 7
+  };
+  
+let fivePlusSeven = () => 5 +7
+
+//C
+// Opdracht: Schrijf een arrow function, op 1 regel (dus met een impliciete return statement), 
+// die de params a en b bij-elkaar optelt. Noem de functie myFunction.
+
+let myFunction = (a, b) => a+b
+
+//D
+// Opdracht: Schrijf een arrow function op 1 regel, die de parameter a behoudt, 
+// en altijd a plus 5 returned. Noem de functie addFive.
+
+let addFive = (a) => a +5
+
+//E
+// Opdracht: Schrijf een arrow function met de naam createObject met een implicit return statement 
+// (dus op 1 regel) die een simpel object returned: {greeting: "hoi"}
+
+let createObject = () => console.log ('greeting: "hoi"')
+//Geen idee....
+
