fad9d414aac918280cd89c641e75afd5287ea9db
BDirkson <69297382+BDirkson@users.noreply.github.com>
2020-11-05 16:40:55 +0100

    Add files via upload

---
+++ JS_Opdracht44/api-client.js
@@ -0,0 +1,40 @@
+//Opdracht : Async/Await
+
+// API-sleutel (v3 auth)
+// e8b51a64b4ff0116ab370b1c20371ee2
+
+// Voorbeeld API-aanvraag
+// https://api.themoviedb.org/3/movie/550?api_key=e8b51a64b4ff0116ab370b1c20371ee2
+
+// API-leestoegangstoken (v4 auth)
+// eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlOGI1MWE2NGI0ZmYwMTE2YWIzNzBiMWMyMDM3MWVlMiIsInN1YiI6IjVmYTE3ZTJiMGMxMjU1MDAzOTI3NDNmOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.EXaCP0vU9lf3kV-HSkRPiRHCQrDgZbyS1P9k-xKMHts
+
+// string interpolation gebruiken om API key en apiUrl samen te voegen - API_KEY toevoegen ?api_key=
+
+const API_KEY = "e8b51a64b4ff0116ab370b1c20371ee2";
+const apiUrl = "https://api.themoviedb.org/3/genre/movie/list";
+
+const apiLink = (`${apiUrl}?api_key=${API_KEY}`);
+console.log (apiLink);
+
+async function getData () {
+    try {
+    const res = await fetch (apiLink, {
+        method: "GET"
+    });
+    const dataMovie = await res.json ();
+        console.log (dataMovie);
+    }   catch (err) {
+        console.log (err + ' fout')
+    }
+}
+
+getData();
+
+
+
+
+
+
+
+
---
+++ JS_Opdracht44/script.js
@@ -0,0 +1,14 @@
+//Opdracht : Async/Await
+
+// API-sleutel (v3 auth)
+// e8b51a64b4ff0116ab370b1c20371ee2
+
+// Voorbeeld API-aanvraag
+// https://api.themoviedb.org/3/movie/550?api_key=e8b51a64b4ff0116ab370b1c20371ee2
+
+// API-leestoegangstoken (v4 auth)
+// eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlOGI1MWE2NGI0ZmYwMTE2YWIzNzBiMWMyMDM3MWVlMiIsInN1YiI6IjVmYTE3ZTJiMGMxMjU1MDAzOTI3NDNmOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.EXaCP0vU9lf3kV-HSkRPiRHCQrDgZbyS1P9k-xKMHts
+
+
+//getData();
+
